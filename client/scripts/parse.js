fcdc7d23cd203b7091312d19973dad45192d0674
Chris Cali <ChristopherCali1415@gmail.com>
2020-08-28 18:17:18 -0400

    Add a POST request to parse.JS and add campus prefix to config.example.js to allow Parse API to call appropriate shared server

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -1,9 +1,27 @@
 var Parse = {
 
+  // NB: Probably just have to adjust CAMPUS to hrr48
   server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
 
   create: function(message, successCB, errorCB = null) {
     // todo: save a message to the server
+    // Copying and pasting in code from Learn
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      // NB: probably need to adjust CAMPUS to hrr48
+      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+
   },
 
   readAll: function(successCB, errorCB = null) {
